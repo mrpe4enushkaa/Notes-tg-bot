732b0733bec5160401ff1b1599bce6c37b995ff7
mrpe4enushkaa_ <daniillavrika1980@gmail.com>
2026-06-17 19:47:25 +0300

    key_bot error

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
 import dotenv from "dotenv";
 import { Bot } from "grammy";
 import { promts } from "./utils/promts";
+import { error } from "node:console";
 
 dotenv.config();
 
-const bot = new Bot(process.env.BOT_KEY!);
+const KEY_BOT = process.env.KEY_BOT;
+
+if (!KEY_BOT) {
+    throw error("Not added KEY_BOT\nLine:10");
+}
+
+const bot = new Bot(KEY_BOT);
 
 bot.command("start", (ctx) => { ctx.reply(promts.start, { parse_mode: "HTML" }) });
 
 bot.start();
-
